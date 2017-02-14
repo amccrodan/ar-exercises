5ecc2e03c58b0724267b8530a8b9c6f328ccf598
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 23:35:29 +0000

    Added check so that only stores with 0 employees can be deleted.

--- lib/store.rb
+++ lib/store.rb
@@ -5,10 +5,16 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
   validate :must_carry_one_clothing_type
 
+  before_destroy :check_no_employees
+
   def must_carry_one_clothing_type
     unless mens_apparel || womens_apparel
       errors.add(:mens_or_womens, "must carry at least one clothing type")
     end
   end
 
+  private
+    def check_no_employees
+      throw :abort if Employee.find_by(store: self.id)
+    end
 end
