3c485dad3046dd07dc18a62d4b802e3ab0fdc905
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 19:49:46 +0000

    Completed sum/count exercises.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+puts "Total annual revenue across stores: " + @total_revenue.to_s
+puts "Average annual revenue across stores: " + (@total_revenue / Store.count).to_s
+puts "Number of stores making over $1M: " + Store.where("annual_revenue > ?", 1000000).count.to_s
