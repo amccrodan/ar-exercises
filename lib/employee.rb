d28ce9377cd262d249c225b1b07d3a16d50aefca
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 23:37:22 +0000

    Changed password assignment to happen after creation.

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,10 +4,11 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, :store_id, presence: true
   validates :hourly_rate, numericality: { greater_than: 39, less_than: 201 }
 
-  before_create :assign_password
+  after_create :assign_password
 
   private
     def assign_password
       self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+      self.save
     end
 end
