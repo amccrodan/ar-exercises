8aa4133710df96658178bd0d4d798e893b15d3ff
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-14 20:35:55 +0000

    Added validations for several fields in each table.

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :must_carry_one_clothing_type
+
+  def must_carry_one_clothing_type
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_or_womens, "must carry at least one clothing type")
+    end
+  end
 
 end
